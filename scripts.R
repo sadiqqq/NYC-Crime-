d0945af3c77e6895e00eefe8a69cb16dc50870ca
sadiqqq <sadeq.salimirad77@gmail.com>
2020-12-05 13:35:39 +0330

    the number of crimes per day counted and stored in a new data frame " manhattan_perday

--- scripts.R
+++ scripts.R
@@ -1,2 +1,9 @@
 # filter main data for Manhattn
-manhattan1<- filter(crime_data, BORO_NM == "MANAHATTAN")
+manhattan1<- filter(crime_data, BORO_NM == "MANHATTAN")
+# counting the number of observations
+manhattan1 %>% add_count(CMPLNT_FR_DT)
+# using chron library for Date variable, 
+library(chron)
+# ex: as.Date('1915-6-16') [1] "1915-06-16"
+# counting the number of crimes per day
+manhattn_perday<- count(manhattan1, CMPLNT_FR_DT)
